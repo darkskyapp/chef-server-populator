3d1d0b1f8c1ac775fed04ec7409c36c7d0e7e6e5
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-03-23 12:11:04 -0400

    Increase verbosity of ChefSpec output
    * Reports are nice
    * The documentation style is also nice

--- test/unit/spec_helper.rb
+++ test/unit/spec_helper.rb
@@ -1,2 +1,10 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+# Generate a report
+ChefSpec::Coverage.start!
+
+RSpec.configure do |c|
+  c.color     = true
+  c.formatter = :documentation
+end
