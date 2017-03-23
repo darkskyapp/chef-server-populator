e1a5e5c547e810740d6fcc3be17ab2d932d5fda0
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-03-23 11:12:51 -0400

    Add source & issues URLs to metadata

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,7 @@ maintainer 'Heavywater'
 maintainer_email 'support@hw-ops.com'
 version '2.0.2'
 
+source_url 'https://github.com/hw-cookbooks/chef-server-populator' if respond_to?(:source_url)
+issues_url 'https://github.com/hw-cookbooks/chef-server-populator/issues' if respond_to?(:issues_url)
+
 depends 'chef-server', '~> 5.0'
