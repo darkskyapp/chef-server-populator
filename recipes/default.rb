4bf816f2ba12117104c8432d52f62c1ec2aa46ca
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-04-03 12:44:07 -0400

    Clarify the restore inclusion in default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,7 @@ else
   include_recipe 'chef-server-populator::client'
 end
 
+# Used when specifying a remote file to restore from backup
 if !node['chef_server_populator']['restore']['file'].empty? &&
    node['chef_server_populator']['restore']['file'] != 'none'
   include_recipe 'chef-server-populator::restore'
