23083b84ab8cc4994aede7ddb5898aac1435cc81
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-08-11 13:28:37 -0400

    Add Consul service definition & Datadog role tag

---
+++ attributes/monitoring.rb
@@ -0,0 +1,14 @@
+node.override['ds_consul']['definitions']['chef-server'] = {
+  type: 'service',
+  parameters: {
+    port: 80,
+    tags: %w(chef-server),
+    check: {
+      interval: '10s',
+      timeout: '5s',
+      http: 'http://127.0.0.1/_status',
+    },
+  },
+}
+
+override['datadog']['tags']['role'] = 'chef-server'
