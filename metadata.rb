ea72bddd4ea4d9ea0eb47b1fee5222fb2343cf5c
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-01-08 17:21:18 -0500

    Bump to v1.3.0

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'ds_chef_server_populator'
 description 'Populate chef server with stuff you want'
 maintainer 'Dark Sky'
 maintainer_email 'jeff@darksky.net'
-version '1.2.2'
+version '1.3.0'
 
 source_url 'https://github.com/darkskyapp/ds_chef_server_populator-cookbook' if respond_to?(:source_url)
 issues_url 'https://github.com/darkskyapp/ds_chef_server_populator-cookbook/issues' if respond_to?(:issues_url)
